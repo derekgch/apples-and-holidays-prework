f5b7f7186b9d491c4189c89266983ff9de3e9ce9
derek chen <derek.g.chen@gmail.com>
2018-03-23 17:29:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # remember to return the updated hash
   holiday_hash.each do |key, dates|
     if key == season
-      dates = {holiday_name=>supply_array}
+      dates = {:holiday_name=>supply_array}
     end
   end
 
