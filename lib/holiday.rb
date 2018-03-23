5adbd6d31ec262d6ca316eed7b8dc5d80b606762
derek chen <derek.g.chen@gmail.com>
2018-03-23 18:26:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_supplies_in_holidays(holiday_hash)
       if supply.size >1
         temp1 = supply.join(", ")
       else
-        temp1 = supply.flatten
+        temp1 = supply.join
       end
 
       puts "  #{temp.capitalize}: #{temp1}"
