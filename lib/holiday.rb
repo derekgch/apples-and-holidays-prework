3a1de2e935eda1887d2bb5d7a89c8d8308706141
derek chen <derek.g.chen@gmail.com>
2018-03-23 18:32:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,16 +79,16 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons,holidays|
     puts "#{seasons.capitalize}:"
     holidays.each do |dates, supply|
-      # temp = dates.to_s
-      # temp = temp.sub(/[_]/, " ")
-      #
-      # if supply.size >1
-      #   temp1 = supply.join(", ")
-      # else
-      #   temp1 = supply.join
-      # end
-      #
-      # puts "  #{temp.capitalize}: #{temp1}"
+      temp = dates.to_s
+      temp = temp.sub(/[_]/, " ")
+      
+      if supply.size >1
+        temp1 = supply.join(", ")
+      else
+        temp1 = supply.join
+      end
+      
+      puts "  #{temp.capitalize}: #{temp1}"
     end
 
   end
