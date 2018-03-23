6ac7eb3007c268b43298ad95f8a4f3c8c690bc41
derek chen <derek.g.chen@gmail.com>
2018-03-23 19:08:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def all_supplies_in_holidays(holiday_hash)
       temp_a.each do |word|
         temp << word.capitalize
       end
-      temp = temp_a.join(" ")
+      
 
       if supply.size >1
         temp1 = supply.join(", ")
