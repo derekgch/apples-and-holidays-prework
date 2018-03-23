7d206100fac6460290e25047eab258a118cbc596
derek chen <derek.g.chen@gmail.com>
2018-03-23 17:58:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |seasons,holidays|
-    puts "#{seasons}:"
+    puts "#{seasons.capitalize}:"
     seasons.each do |dates, supply|
       puts "#{dates}: #{supply.join(",")}"
     end
